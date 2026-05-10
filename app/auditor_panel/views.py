c65bfd63c3d4229f1cff7f59cecddf8f324c2c29
Shmadik <necmnou@gmail.com>
2026-05-10 18:42:17 +0300

    add new views for crud

--- app/auditor_panel/views.py
+++ app/auditor_panel/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from .models import *
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, View
 from django.urls import reverse_lazy
+from rest_framework.views import APIView
+from rest_framework import serializers
+
+
+class IpPoolBaseView:
+    model = IpPool
+    fields = ['ip', 'description', 'ports', 'mask']
+    success_url = reverse_lazy("auditor")
+
+
 
 class IpPoolRead(ListView):
     model = IpPool
@@ -9,10 +19,8 @@ class IpPoolRead(ListView):
     context_object_name = 'ip_pools'
 
 
-class IpPoolCreate(CreateView):
-    model = IpPool
-    fields = ['ip', 'description', 'ports', "mask"]
-    success_url = reverse_lazy("auditor")
+class IpPoolCreate(IpPoolBaseView, CreateView):
+    pass
 
 
 class IpPoolDetail(DetailView):
@@ -20,8 +28,12 @@ class IpPoolDetail(DetailView):
     pk_url_kwarg = 'id'
 
 
+class IpPoolUpdate(IpPoolBaseView, UpdateView):
+    pk_url_kwarg = 'id'
 
 
 class IpPoolDelete(DeleteView):
     model = IpPool
+    pk_url_kwarg = 'id'
     success_url = reverse_lazy("auditor")
+
