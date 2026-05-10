141f86669c8f45f4baf51fe02720b4bb6904f5e4
Shmadik <necmnou@gmail.com>
2026-05-10 20:03:39 +0300

    add post view api for starting connection with mas

--- app/admin_panel/views.py
+++ app/admin_panel/views.py
@@ -2,12 +2,26 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, CreateView, UpdateView, DeleteView, ListView
 # Create your views here.
 from auditor_panel import models
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from .serializer import IpSerializer
 
-
-
-class panelView(ListView):
+class PanelView(ListView):
     model = models.IpPool
     template_name = 'panel.html'
     context_object_name = 'ip_pools'
+
+
+class IpRun(APIView):
+    def post(self, request, *args, **kwargs):
+        Serializer = IpSerializer(data=request.data)
+        if Serializer.is_valid():
+           print(Serializer.data)
+           return Response(Serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(Serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 
 
