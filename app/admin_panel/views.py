38e7811eb30466d828921cdca32ef626af012766
Shmadik <necmnou@gmail.com>
2026-05-10 18:40:51 +0300

    Add view for short represenatation of Ip model

--- app/admin_panel/views.py
+++ app/admin_panel/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView, UpdateView, DeleteView
+from django.views.generic import TemplateView, CreateView, UpdateView, DeleteView, ListView
 # Create your views here.
+from auditor_panel import models
 
 
-class panelView(TemplateView):
+
+class panelView(ListView):
+    model = models.IpPool
     template_name = 'panel.html'
+    context_object_name = 'ip_pools'
 
 
