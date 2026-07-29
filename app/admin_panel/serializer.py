0dab2bf937d2ede64241e510cf7c09a9dbd4fdda
Shmadik <necmnou@gmail.com>
2026-07-29 16:54:36 +0300

    start project from zero again

--- app/admin_panel/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from auditor_panel import models
-
-
-class IpSerializer(ModelSerializer):
-    class Meta:
-        model = models.IpPool
-        fields = '__all__'
--- app/auditor_panel/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AuditorPanelConfig(AppConfig):
-    name = 'auditor_panel'
