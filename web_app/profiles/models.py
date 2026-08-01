8f9fcc2f5b7ceb147cc637ea6ae869b96c3373f5
Shmadik <necmnou@gmail.com>
2026-08-01 19:23:44 +0300

    add model for setting

--- web_app/profiles/models.py
+++ web_app/profiles/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class UserSetting(models.Model):
+    language = models.CharField(max_length=3)
+    site_theme = models.CharField(max_length=30)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
