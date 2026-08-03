0f91182318080eb9c5c2cbdef4c8cefbf79c0a10
Shmadik <necmnou@gmail.com>
2026-08-03 23:24:38 +0300

    add user id column

--- web_app/main_page/models.py
+++ web_app/main_page/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from django.db.models import CASCADE
 from dns.rdatatype import NULL
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class IpTable(models.Model):
     ip = models.GenericIPAddressField()
     subnet = models.GenericIPAddressField()
+    user_id = models.ForeignKey(User, on_delete=CASCADE, unique=False, null=False)
 
 class ScanResult(models.Model):
     title = models.CharField(max_length=35)
