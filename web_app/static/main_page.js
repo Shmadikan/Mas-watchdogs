422ab1fd28708bf3526dff1ffb6e7221b7cbabe8
Shmadik <necmnou@gmail.com>
2026-08-04 20:41:02 +0300

    redesign main page with results panel

---
+++ web_app/static/main_page.js
@@ -0,0 +1,68 @@
+const select_all = document.querySelector("#select-button")
+const analysis = document.querySelector("#analysis-button")
+const get_scan_result_url = document.body.getAttribute("data-get-url")
+
+function get_result_data() {
+    fetch(get_scan_result_url).then(
+        (response) => response.json()
+    ).then(
+        (data) => {
+            const tbody = document.querySelector('#results-body')
+            tbody.innerHTML = ''
+
+            if (!data.length) {
+                tbody.innerHTML = '<tr><td>Results are empty</td></tr>'
+                return
+            }
+
+            data.forEach((item) => {
+                const tr = document.createElement('tr')
+                tr.dataset.id = item.id
+                const td = document.createElement('td')
+                td.innerHTML = item.title + ' ' + item.date + ' ' + item.desc
+                tr.appendChild(td)
+                tbody.appendChild(tr)
+            })
+        }
+    )
+}
+
+
+
+select_all.onclick = function (event) {
+    const checkboxes_ip = document.querySelectorAll('.ip-list')
+    for (checkbox of checkboxes_ip) {
+        checkbox.checked = true
+    }
+}
+
+analysis.onclick = function (event) {
+    const tables = document.querySelectorAll('#ip-table')
+    let list_ip = []
+    for (table of tables) {
+        if (table.children[2].children[0].checked) {
+            struct = new Map
+            struct["ip"] = table.children[0].children[0].text
+            struct["subnet"] = table.children[1].children[0].text
+            struct["id"] = table.children[0].children[0].getAttribute('data-user-id')
+            list_ip.push(struct)
+        }
+    }
+    url = analysis.getAttribute('data-url')
+    
+    csrf_token = analysis.getAttribute('data-token')
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(list_ip),
+        headers: {
+            'X-CSRFToken': csrf_token
+        }
+    }).then(
+        (response)=>{
+            get_result_data()
+        }
+    ).catch(()=>{console.log("error")})
+}
+
+
+get_result_data()
