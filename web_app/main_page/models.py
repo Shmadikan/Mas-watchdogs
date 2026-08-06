7e5fdf4bdde387802ccf7363d4a160877c549a1e
Shmadik <necmnou@gmail.com>
2026-08-06 19:26:00 +0300

    add ip transform and result formatting to models

--- web_app/main_page/models.py
+++ web_app/main_page/models.py
@@ -10,6 +10,11 @@ class IpTable(models.Model):
     subnet = models.GenericIPAddressField()
     user_id = models.ForeignKey(User, on_delete=CASCADE, unique=False, null=False)
 
+    @classmethod
+    def ipv4_transform(cls, ip: str, subnet: str) -> str:
+        bit_octet = sum(bin(int(octet)).count('1') for octet in subnet.split('.'))
+        return f"{ip}/{bit_octet}"
+
 class ScanResult(models.Model):
     title = models.CharField(max_length=35)
     description = models.TextField()
@@ -22,4 +27,25 @@ class ScanResult(models.Model):
         for data in ip_list:
             scan = ScanResult(title="", description="", date=now(), ip_fk=IpTable.objects.get(id=data['id']))
             scan.save()
+
+    @classmethod
+    def all_formating(cls):
+        data = ScanResult.objects.all()
+        return list(map(lambda x: {
+            'id': x.pk,
+            'date': x.date.strftime('%Y.%m.%d %H:%M'),
+            'title': x.title,
+            'desc': x.description
+        }, data))
+
+    @classmethod
+    def get_format(clm, **kwargs):
+        id = kwargs['id']
+        result = ScanResult.objects.get(id=id)
+        return {
+            'id': result.pk,
+            'date': result.date.strftime('%Y.%m.%d %H:%M'),
+            'title': result.title,
+            'desc': result.description
+        }
 
