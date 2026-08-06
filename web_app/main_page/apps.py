6a7b1d852b10a348975168e59bd0afe68789d7ae
Shmadik <necmnou@gmail.com>
2026-08-06 19:25:57 +0300

    subscribe to redis pubsub on app startup

--- web_app/main_page/apps.py
+++ web_app/main_page/apps.py
@@ -1,5 +1,15 @@
 from django.apps import AppConfig
-
+import redis
+from redis.client import PubSub
 
 class MainPageConfig(AppConfig):
     name = 'main_page'
+    redis_client = None
+    pubsub: PubSub = None
+
+    def ready(self):
+        MainPageConfig.redis_client = redis.client.Redis()
+        MainPageConfig.pubsub = MainPageConfig.redis_client.pubsub()
+        MainPageConfig.pubsub.subscribe('service_answer')
+
+
