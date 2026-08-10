237c7f62577290bcb5e18433e44fe029b67fee02
Shmadik <necmnou@gmail.com>
2026-08-10 21:03:29 +0300

    add redis client to profiles app config

--- web_app/profiles/apps.py
+++ web_app/profiles/apps.py
@@ -1,5 +1,9 @@
 from django.apps import AppConfig
-
+import redis
 
 class ProfilesConfig(AppConfig):
     name = 'profiles'
+    redis_client: redis.Redis | None = None
+
+    def ready(self):
+        ProfilesConfig.redis_client = redis.client.Redis()
