23a39a575e2f3ac6707f81ae9210025c8f1bf526
Shmadik <necmnou@gmail.com>
2026-08-10 20:46:29 +0300

    add theme cookie script

---
+++ web_app/static/theme_script.js
@@ -0,0 +1,8 @@
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+document.documentElement.setAttribute('data-theme', getCookie('theme'))
