3817cb62b0c7236b012c35eae984bb03dad36769
Shmadik <necmnou@gmail.com>
2026-08-25 18:19:19 +0300

    get keys in web app

--- web_app/main_page/apps.py
+++ web_app/main_page/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+import os
 import redis
 from redis.client import PubSub
 
@@ -8,7 +9,10 @@ class MainPageConfig(AppConfig):
     pubsub: PubSub = None
 
     def ready(self):
-        MainPageConfig.redis_client = redis.client.Redis()
+        MainPageConfig.redis_client = redis.client.Redis(
+            host=os.getenv("REDIS_HOST", "localhost"),
+            port=int(os.getenv("REDIS_PORT", "6379")),
+        )
         MainPageConfig.pubsub = MainPageConfig.redis_client.pubsub()
         MainPageConfig.pubsub.subscribe('service_answer')
 
